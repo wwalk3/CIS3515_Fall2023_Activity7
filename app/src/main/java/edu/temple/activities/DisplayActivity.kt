414640d55e32ddb208795ba72551ca61e353d77c
wwalk <wwalk831@gmail.com>
2023-10-05 14:46:06 -0400

    started on the adapter that calls TextSizeAdapter in order to pass data back to TextSizeActivity

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 
+const val RESULT_KEY = "sending a message"
 class DisplayActivity : AppCompatActivity() {
 
     // TODO Step 1: Launch TextSizeActivity when button clicked to allow selection of text size value
--- app/src/main/java/edu/temple/activities/TextSizeActivity.kt
+++ app/src/main/java/edu/temple/activities/TextSizeActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -24,7 +25,8 @@ class TextSizeActivity : AppCompatActivity() {
 
             // TODO Step 2: Pass selected value back to activity that launched TextSizeActivity
             adapter = TextSizeAdapter(textSizes){
-
+                textSizes.toString().toFloat()
+                setResult(RESULT_OK, Intent().putExtra(RESULT_KEY, textSizes.toString().toFloat()))
             }
             layoutManager = LinearLayoutManager(this@TextSizeActivity)
         }
