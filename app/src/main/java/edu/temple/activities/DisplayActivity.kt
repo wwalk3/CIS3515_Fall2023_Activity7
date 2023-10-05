ee2e543aab30fbbc7516fa655e5c9910da033a70
wwalk <wwalk831@gmail.com>
2023-10-05 14:32:50 -0400

    Set up launcher in DisplayActivity, started button onSetClickListener in order to retrieve textSize

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -1,14 +1,25 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 
 class DisplayActivity : AppCompatActivity() {
 
     // TODO Step 1: Launch TextSizeActivity when button clicked to allow selection of text size value
 
+
+        val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+
+        }
+
+    findViewById<Button>(R.id.textSizeSelectorButton).setOnClickListener {
+            val launchIntent = Intent(this@MainActivity, TextSizeActivity::class.java)
+
+        }
     // TODO Step 3: Use returned value for lyricsDisplayTextView text size
 
     private lateinit var lyricsDisplayTextView: TextView
